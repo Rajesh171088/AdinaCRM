393e3bbc73e4f975ed010cac0f9f87c3749cd60f
Rajesh171088 <rajesh171088@gmail.com>
2023-08-22 09:39:04 +0530

    First commit

---
+++ src/test/java/sample/Ex1.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		int age=25;
+		if(age>24)
+		{
+			System.out.println("Approve");
+		}
+		else
+			System.out.println("Reject");
+	}
+
+}
